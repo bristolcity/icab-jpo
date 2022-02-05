db975362984df9253f3a37a580733a59e6788dc1
brice simo <simobrice6@gmail.com>
2022-02-05 14:26:41 +0100

    page d'animation okay

--- app/src/main/java/cm/sigeris/icab/MainActivity.java
+++ app/src/main/java/cm/sigeris/icab/MainActivity.java
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView logo;
+    private static int splashTimeOut = 6000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Handler handler = new Handler();
+        logo = findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,Accueil.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.myanim);
+        logo.startAnimation(myanim);
+    }
+        /*Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,5 +38,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }, 5000L);
-    }
+    }*/
 }
