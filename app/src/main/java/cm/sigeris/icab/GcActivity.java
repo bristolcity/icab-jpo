1b478f23beb187d38c34b8f2a7d4fe640f2d858b
yvansimo <ysimoguonmadje@gmail.com>
2022-02-06 15:47:01 +0100

    Les pages de consultations des filières + les ajustements sur photo

--- app/src/main/java/cm/sigeris/icab/GcActivity.java
+++ app/src/main/java/cm/sigeris/icab/GcActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ImageView;
 
 public class GcActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class GcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gc);
+        ImageView myImage = (ImageView) findViewById(R.id.imageView6);
+        myImage.setAlpha(0.8f);
 
     }
 
