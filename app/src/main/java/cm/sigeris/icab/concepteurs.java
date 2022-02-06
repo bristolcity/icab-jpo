51586ce37b7b218f800368a0fc54f14d1fabecab
keryljalbred <jalbredkeryldjeukouatchani@gmail.com>
2022-02-06 11:06:21 +0100

    premiere modification et animation

---
+++ app/src/main/java/cm/sigeris/icab/concepteurs.java
@@ -0,0 +1,14 @@
+package cm.sigeris.icab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class concepteurs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_concepteurs);
+    }
+}
