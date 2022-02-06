c1587f50fe02feadba4f25235a91cbb8a63d9886
keryljalbred <jalbredkeryldjeukouatchani@gmail.com>
2022-02-06 14:59:44 +0100

    premier test modification

--- app/src/main/java/cm/sigeris/icab/MainActivity.java
+++ app/src/main/java/cm/sigeris/icab/MainActivity.java
@@ -2,21 +2,41 @@ package cm.sigeris.icab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView logo;
     private static int splashTimeOut = 6000;
+    private TextView gl2,superv,teacher,textView3,textView2;
+    int doplay = 2000;
+    Handler handler;
+    ArrayList<String> arrMessage = new ArrayList<>();
+    int position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logo = findViewById(R.id.logo);
+        logo = findViewById(R.id.logo);
+        gl2 = findViewById(R.id.gl2);
+        //teacher = findViewById(R.id.teacher);
+        //superv = findViewById(R.id.superv);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        //animer texte
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -28,7 +48,19 @@ public class MainActivity extends AppCompatActivity {
         },splashTimeOut);
 
         Animation myanim = AnimationUtils.loadAnimation(this,R.anim.myanim);
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.anim2);
+        /*ObjectAnimator moveAnim = ObjectAnimator.ofFloat(teacher, "Y", 1350);
+        moveAnim.setDuration(2000);
+        moveAnim.setInterpolator(new BounceInterpolator());
+        moveAnim.start();*/
+        ObjectAnimator scaleAnim = ObjectAnimator.ofFloat(textView2, "scaleX", 1.0f, 2.0f);
+        scaleAnim.setDuration(3000);
+        scaleAnim.setRepeatCount(ValueAnimator.INFINITE);
+        scaleAnim.setRepeatMode(ValueAnimator.REVERSE);
+        scaleAnim.start();
+
         logo.startAnimation(myanim);
+        gl2.startAnimation(anim);
     }
         /*Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
--- app/src/main/java/cm/sigeris/icab/icab.java
+++ app/src/main/java/cm/sigeris/icab/icab.java
@@ -1,14 +1,49 @@
 package cm.sigeris.icab;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class icab extends AppCompatActivity {
+    private TextView textView3,textView2,textView,textView5,textView6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icab);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView5 = findViewById(R.id.textView5);
+        textView6 = findViewById(R.id.textView6);
+        textView3 = findViewById(R.id.textView3);
+
+        /*ObjectAnimator anim = ObjectAnimator.ofFloat(textView5, "rotation", 10f, 12f);
+        anim.setDuration(2000);                  // Duration in milliseconds
+         // E.g. Linear, Accelerate, Decelerate
+        anim.start();*/
+        @SuppressLint("ResourceType") Animation myanim = AnimationUtils.loadAnimation(this,R.anim.myanim);
+
+        ObjectAnimator anima = ObjectAnimator.ofFloat(textView3, "rotation", 360);
+        anima.setDuration(2000);
+        anima.start();
+
+        ObjectAnimator anime = ObjectAnimator.ofFloat(textView6, "rotation", 360);
+        anime.setDuration(2000);
+        anime.start();
+        ObjectAnimator anim = ObjectAnimator.ofFloat(textView, "rotation", 360);
+        anim.setDuration(2000);
+        anim.start();
+        textView5.startAnimation(myanim);
+        textView2.startAnimation(myanim);
+
+
     }
+
 }
+
